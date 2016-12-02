56d64daa29459a25875afbbe1e218ab46d8cd5ca
Joao Souza <joao.souza@ice.ufjf.br>
2016-12-02 14:11:43 -0200

    Adicionado arquivos web

---
+++ src/main/webapp/resources/js/validaCampos.js
@@ -0,0 +1,13 @@
+function desabilitaEscrita(e){
+	var tecla=new Number();
+	do{		
+		return false;
+		
+	}while(tecla!=0);
+			
+}
+
+
+function setfocus(id){  
+	document.getElementById(id).focus() ; 
+	}  
